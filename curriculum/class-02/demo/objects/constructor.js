74c761e9bbe3265ff20c4cd754fa06bf0ad60ecc
Keith Dahlby <keith@newbo.co>
2019-05-21 12:37:28 -0500

    Class 2 demo

--- curriculum/class-02/demo/objects/constructor.js
+++ curriculum/class-02/demo/objects/constructor.js
@@ -1,27 +1,36 @@
 'use strict';
 
-// Animal Constructor
 const Animal = function(name) {
   this.name = name;
-};
+  console.log('this inside Animal', this);
+}
 
-// Animal's walk prototype method
+/*
+// We would have done this pre-const
+function Animal(name) {
+  this.name = name;
+}
+*/
+
 Animal.prototype.walk = () => {
-  console.log('Walking ...');
-};
+  console.log('Walking...');
+  console.log('this in arrow function', this);
+  // Can't use this because we have an arrow function
+}
 
-// Dog Constructor
 const Dog = function(name) {
-  // Inherit/Become the Animal constructor
   Animal.call(this, name);
-};
+  console.log('this inside Dog', this);
+}
 
-// Attach esoteric props and methods to the dog prototype
 Dog.prototype = new Animal();
 
-// Dog specific prototype methods
-Dog.prototype.speak = () => {
-  console.log('WOOF!');
-};
+Dog.prototype.speak = function() {
+  console.log(`My name is ${this.name}`);
+}
+
+Dog.prototype.toString = function() {
+  return `Dog: ${this.name}`;
+}
 
 module.exports = Dog;
--- curriculum/class-02/demo/objects/factory.js
+++ curriculum/class-02/demo/objects/factory.js
@@ -1,24 +1,49 @@
 'use strict';
 
-const Animal = () => ({
-  walk: () => {
-    console.log('Walking ...');
-  },
-});
+const animalFactory = (name) => {
+  console.log("Animal factory this", this);
+  return {
+    name,
+    //name: name,
+    walk: () => {
+      console.log('Walking...');
+      console.log('this in arrow function', this);
+    }
+  };
+};
 
-function DogFactory(name) {
+const dogFactory = function(name) {
+  console.log('dogFactory!')
+
   let dog = Object.assign(
-    {},
-    {name},
-    {speak},
-    Animal()
+    animalFactory(name),
+    { speak },
+    {
+      toString: function() {
+        return `Dog: ${this.name}`;
+      }
+    },
+    { hungry: true }
   );
 
-  function speak () { console.log('WOOF!'); }
+  return runMixin(dog);
 
-  return Object.freeze(dog);
+  // var dog = animalFactory(name);
+  // dog.speak = speak;
+  // return dog;
+
+  function speak(toMe) {
+    console.log(`My name is ${this.name}` +
+      (toMe ? `, ${toMe}` : ''));
+  }
 }
 
-module.exports = DogFactory;
+function runMixin(animal) {
+  animal.run = function(howFast) {
+    console.log(`Running ${howFast} fast!`);
+  }
 
+  return animal;
+}
 
+module.exports = dogFactory;
