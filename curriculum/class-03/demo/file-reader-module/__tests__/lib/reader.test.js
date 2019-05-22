5d13c0e2bbfd53c9977d5b473a641ed0d01cdfd4
Keith Dahlby <keith@newbo.co>
2019-05-22 12:13:57 -0500

    Implemented callback-based reader

--- curriculum/class-03/demo/file-reader-module/__tests__/lib/reader.test.js
+++ curriculum/class-03/demo/file-reader-module/__tests__/lib/reader.test.js
@@ -5,11 +5,23 @@ const reader = require('../../lib/reader.js');
 describe('File Reader Module', () => {
 
   it('can read a file', done => {
-    let file = `${__dirname}/../data/file.txt`;
+    let file = `${__dirname}/../../data/file.txt`;
+    // let file = `./data/file.txt`; // This works, too, but only by luck
+    console.log(file);
 
     reader(file, (err, data) => {
+      expect(err).toBeNull();
       expect(data).toBeDefined();
       done();
     });
   });
+
+  it('calls callback with error for bad file', done => {
+    let file = `${__dirname}/../../data/missing.txt`;
+
+    reader(file, (err) => {
+      expect(err).toBeDefined();
+      done();
+    })
+  })
 });
--- curriculum/class-03/demo/file-reader-module/lib/reader.js
+++ curriculum/class-03/demo/file-reader-module/lib/reader.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const fs = require('fs');
+
 module.exports = function (filename, callback) {
-  callback(null, 'data!');
+  fs.readFile(filename, (err, data) => {
+    if (err) {
+      console.error(err);
+      return callback(err);
+    }
+
+    console.log(data.toString());
+
+    callback(null, data);
+  });
 };
